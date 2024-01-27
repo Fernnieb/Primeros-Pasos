befed23b64f0420cf711edd08389b7199877748d
Fernanda Bustos Reyes <Fernanda Bustos>
2024-01-27 10:07:13 -0300

    Ingreso variables

--- siwtch.java
+++ siwtch.java
@@ -1,9 +1,14 @@
-
+import javax.swing.*;
+import java.lang.*;
 public class siwtch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int H,B,tw,tf;
+		H=Integer.parseInt(JOptionPane.showInputDialog("Ingresa la altura:"));
+		B=Integer.parseInt(JOptionPane.showInputDialog("Ingresa el ancho:"));
+		tw=Integer.parseInt(JOptionPane.showInputDialog("Ingresa espesor del alma:"));
+		tf=Integer.parseInt(JOptionPane.showInputDialog("Ingresa espesor del ala:"));
 	}
 
 }
